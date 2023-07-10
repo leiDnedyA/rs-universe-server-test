8286c5d9d1596848b3df91e1e0149bbe0561876b
Ayden Diel <aydendiel@gmail.com>
2023-07-10 11:56:32 -0400

    Implemented bouncing ball client, except for server functions

--- src/app.rkt
+++ src/app.rkt
@@ -2,13 +2,32 @@
 (require "universe-server.rkt"
          racketscript/htdp/image)
 
-(define (tick w) 
-    ; (#js*.console.log w)
-    (+ w 1))
+;; Implementation of bouncing ball example from
+;; 2htdp/universe docs
+;; ( https://docs.racket-lang.org/teachpack/2htdpuniverse.html )
+;; (ctrl + F search for "Designing the Ball World")
 
-; (define (draw w)
-;     (rectangle 200 200 'solid 'blue))
+(define SPEED 5)
+(define RADIUS 10)
+(define WORLD0 300)
 
-; (big-bang 0 [on-tick tick] [to-draw draw])
+(define WIDTH 600)
+(define HEIGHT 400)
+(define MT (empty-scene WIDTH HEIGHT))
+(define BALL (circle RADIUS 'solid 'blue))
 
-(universe 0 [u-on-tick tick])
+(define (move ws)
+  (define is-active (number? ws))
+  (if is-active
+    (if (<= ws 0)
+        'RESTING
+        (- ws SPEED))
+    ws))
+
+(define (draw ws)
+  (cond
+    [(number? ws) (underlay/xy MT 50 ws BALL)]
+    [(symbol? ws) (underlay/xy MT 50 50 (text "Resting" 24 'blue))]))
+
+(big-bang WORLD0 [on-tick move] [to-draw draw])
+; (universe 0 [u-on-tick tick])
