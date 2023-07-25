f3d3798e14723ea9d957f32e4e8ce758ba28d09f
Ayden Diel <aydendiel@gmail.com>
2023-07-25 12:58:48 -0400

    implemented encoding/decoding for s expression data for peer sending

--- src/chat-room/util.rkt
+++ src/chat-room/util.rkt
@@ -35,3 +35,42 @@
                        0
                        (string->list str)))
   (* (/ width 1000) font-size))
+
+(define (js-string? s)
+  (string? ($/str s)))
+
+(define (encode data)
+  (cond [(list? data) (foldl (lambda (curr result)
+                               (#js.result.push (encode curr))
+                               result)
+                      ($/array)
+                      data)]
+        [(number? data)    ($/obj [type #js"number"]
+                                  [val data])]
+        [(string? data)    ($/obj [type #js"string"]
+                                  [val (js-string data)])]
+        [(symbol? data)    ($/obj [type #js"symbol"]
+                                  [val (js-string (symbol->string data))])]
+        [(boolean? data)   ($/obj [type #js"boolean"]
+                                  [val data])]
+        [(js-string? data) ($/obj [type #js"js-string"]
+                                  [val data])]
+        [else              (begin 
+                             (#js*.console.warn data-type-warning)
+                             ($/obj [type #js"unknown"]
+                                  [val data]))]))
+
+(define data-type-warning #js"racketscript/htdp/universe: Unsupported datatype being passed to/from server.")
+
+(define (decode data)
+  (cond [(#js*.Array.isArray data) (#js.data.reduce (lambda (result curr)
+                                                      (append result (list (decode curr))))
+                                                    '())]
+        [($/binop == #js.data.type #js"number")    #js.data.val]
+        [($/binop == #js.data.type #js"string")    (js-string->string #js.data.val)]
+        [($/binop == #js.data.type #js"symbol")    (string->symbol (js-string->string #js.data.val))]
+        [($/binop == #js.data.type #js"boolean")   #js.data.val]
+        [($/binop == #js.data.type #js"js-string") #js.data.val]
+        [($/binop == #js.data.type #js"unknown")   (begin
+                                                     (#js*.console.warn data-type-warning)
+                                                     #js.data.val)]))
